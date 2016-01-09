e9a5f9e6cb3530df24bf0345217656531225eed8
Tomasz Marzeion <szafir1100@gmail.com>
2016-01-09 20:34:36 +0100

    added loggers BufferedWriter append mode function

--- src/com/company/Logger.java
+++ src/com/company/Logger.java
@@ -2,10 +2,7 @@ package com.company;
 
 import com.sun.istack.internal.Nullable;
 
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 /**
  * Created by tomek on 09.01.16.
@@ -16,7 +13,6 @@ public class Logger {
     private static final String TASK_LOG_TAG = "T";
     private static final String PROJECT_LOG_TAG = "P";
     private static final String APP_LOG_TAG = "A";
-
 
     private static Logger singleton;
 
@@ -54,15 +50,19 @@ public class Logger {
      */
     private void writeToLogTest(String logMessage) {
 
-        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(LOG_FILE_PATH), "utf-8"))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOG_FILE_PATH, isThereLogFile(LOG_FILE_PATH)))) {
             writer.write(logMessage);
             writer.newLine();
+            writer.close();
             System.out.println("success");
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    private boolean isThereLogFile(String filePath) {
+        File f = new File(filePath);
+        return f.exists() && !f.isDirectory();
     }
 
     private String formatLog(String symbol, @Nullable String name, String message) {
